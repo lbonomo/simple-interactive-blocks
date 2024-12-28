561130b18acb476f3ac5a56f71c4686f3e216760
Lucas Bonomo <bonomo.lucas@gmail.com>
2024-12-28 10:12:27 -0300

    Adding phpcs and phpmd

--- src/render.php
+++ src/render.php
@@ -7,6 +7,7 @@
  *     $content (string): The block default content.
  *     $block (WP_Block): The block instance.
  *
+ * @package simple-interactive-blocks
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
@@ -18,24 +19,24 @@ $id_result = wp_unique_id( 'result-' );
 wp_interactivity_state(
 	'simple-interactive-blocks',
 	array(
-		'result'	=> array(),
-		'term'		=> ''
+		'result' => array(),
+		'term'   => '',
 	)
 );
 
-$content = array( 
+$content = array(
 	'showResult' => false,
-	'inputID' => $id_input,
-	'resultID' => $id_result,
-	'posts' => array()
+	'inputID'    => $id_input,
+	'resultID'   => $id_result,
+	'posts'      => array(),
 );
 
 ?>
 
 <div
-	<?php echo get_block_wrapper_attributes(); ?>
+	<?php echo esc_attr( get_block_wrapper_attributes() ); ?>
 	data-wp-interactive="simple-interactive-blocks"
-	<?php echo wp_interactivity_data_wp_context( $content ); ?>
+	<?php echo esc_attr( wp_interactivity_data_wp_context( $content ) ); ?>
 >
 	<form 
 		role="search" 
